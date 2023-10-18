bf8129c45a09e059479ba42712dcf2e3e1a69836
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-10-18 22:07:30 +0400

    find url's

--- Lab1/pics.py
+++ Lab1/pics.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import json
 
-def createDir():
+def createDir(img_class):
     if not os.path.isdir("dataset"):
-        os.mkdir("dataset")
-    os.mkdir("dataset/cats")
-    os.mkdir("dataset/dogs")
+       os.mkdir("dataset")
+    os.mkdir(f"dataset/{img_class}")
 
-createDir()
+def yandexSearch(img_class):
+    url = "https://yandex.ru/images/search?text={img_class}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    image_tags = soup.find_all('img')
+
+    image_urls = [img['src'] for img in image_tags]
+    print(image_urls)
+
+yandexSearch("cats")
