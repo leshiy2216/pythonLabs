c5dadcb8f33aef48af2d15f8f5fd066fa9992db7
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-11-14 18:49:07 +0400

    added argparse

--- Lab1/pics.py
+++ Lab1/pics.py
@@ -2,26 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import logging
+import argparse
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def create_dir(img_class):
+def create_dir(folder_path):
     try:
-        folder_path = os.path.join("dataset", img_class)
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
     except Exception as e:
         logger.exception("Can't create a folder")
 
 
-def img_download(img_class):
+def img_download(img_class, folder_path, num_images):
     create_dir(img_class)
 
     page = 1
-    img_num = 0
+    k = 0
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -31,7 +31,7 @@ def img_download(img_class):
         "Referer": "https://www.google.com/",
         "Accept-Encoding": "gzip, deflate, br",
         }
-    while img_num < 1000:
+    while k < num_images:
         url = f"https://www.bing.com/images/search?q={img_class}&first={page}"
         try:
             response = requests.get(url, headers)
@@ -42,7 +42,7 @@ def img_download(img_class):
             for img_url in image_urls:
                 try:
                     response = requests.get(img_url)
-                    filename = os.path.join("dataset", img_class, f"{img_num:04}.jpg")
+                    filename = os.path.join(folder_path, img_class, f"{img_num:04}.jpg")
 
                     with open(filename, 'wb') as f:
                         f.write(response.content)
@@ -55,5 +55,11 @@ def img_download(img_class):
 
 
 if __name__=="__main__":
-    img_download("cats")
-    img_download("dogs")
+    parser = argparse.ArgumentParser(description='Download images from Bing Images')
+    parser.add_argument('img_class', type=str, help='Name of the image class (e.g., "cats")')
+    parser.add_argument('folder_path', type=str, help='Output directory for saving images')
+    parser.add_argument('num_images', type=int, help='Number of images to download')
+
+    
+    args = parser.parse_args()
+    img_download(args.img_class, args.folder_path, args.num_images)
