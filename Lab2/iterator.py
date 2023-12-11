c78e0e115a12e78625107a20f7b3f7d73ea7d2c4
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-12-11 16:36:52 +0400

    iterator changes

--- Lab2/iterator.py
+++ Lab2/iterator.py
@@ -1,72 +1,66 @@
 import argparse
-import os
+import csv
 
 
 class DatasetIterator:
-    def __init__(self, dataset_path: str, class_name: str):
-        self.dataset_path = dataset_path
+    def __init__(self, annotation_file: str, class_name: str):
+        self.annotation_file = annotation_file
         self.class_name = class_name
-        self.class_instances = self._get_class_instances()
+        self.class_instances = None
         self.current_index = 0
+
+    def __iter__(self):
+        self.class_instances = self._get_class_instances()
+        return self
 
     def _get_class_instances(self):
         instances = []
-        class_folder = os.path.join(self.dataset_path, self.class_name)
-
-        if os.path.exists(class_folder) and os.path.isdir(class_folder):
-            instances = [os.path.join(class_folder, filename) for filename in os.listdir(class_folder)]
-
+        with open(self.annotation_file, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            for row in csvreader:
+                if len(row) == 3 and row[2] == self.class_name:
+                    instances.append(row[0])
         return instances
 
-    def get_next_instance(self):
+    def __next__(self):
         if self.current_index < len(self.class_instances):
             next_instance = self.class_instances[self.current_index]
             self.current_index += 1
             return next_instance
         else:
-            return None
+            raise StopIteration
 
 
 class ClassIterator:
-    def __init__(self, dataset_path: str, class_names: list):
-        self.dataset_path = dataset_path
+    def __init__(self, annotation_file: str, class_names: list):
+        self.annotation_file = annotation_file
         self.class_names = class_names
-        self.dataset_iterators = {name: DatasetIterator(dataset_path, name) for name in class_names}
         self.current_name_index = 0
 
-    def next_image(self):
-        """
-        function get path to next image
-        Parameters
-        ----------
-        """
-        if not self.class_names or self.current_name_index >= len(self.class_names):
-            return None
+    def __iter__(self):
+        return self
 
-        current_class_name = self.class_names[self.current_name_index]
-        current_dataset_iterator = self.dataset_iterators[current_class_name]
-        next_instance = current_dataset_iterator.get_next_instance()
-
-        if next_instance is not None:
-            return next_instance
-        else:
+    def __next__(self):
+        if self.current_name_index < len(self.class_names):
+            current_class_name = self.class_names[self.current_name_index]
             self.current_name_index += 1
-            return self.next_image()
+            return DatasetIterator(self.annotation_file, current_class_name)
+        else:
+            raise StopIteration
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process a dataset.')
-    parser.add_argument('dataset_path', type=str, help='Path to the dataset')
+    parser = argparse.ArgumentParser(description='Process a dataset')
+    parser.add_argument('annotation_file', type=str, help='Path to annotation')
     parser.add_argument('class_names', type=str, nargs='+', help='List of class names')
 
     args = parser.parse_args()
 
-    dataset_path = args.dataset_path
+    annotation_file = args.annotation_file
     class_names = args.class_names
 
-    iterator = ClassIterator(dataset_path, class_names)
+    iterator = ClassIterator(annotation_file, class_names)
 
-    instance = iterator.next_image()
-    while instance is not None:
-        print(instance)
-        instance = iterator.next_image()
+    for dataset_iterator in iterator:
+        for instance in dataset_iterator:
+            print(instance)
